79fd1de4de1593c4c22beb3d263c7efe34d2b269
apexlawndivya <djanga@apexlawncompany.com>
2025-02-06 17:12:41 +0530

    spell correct1

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "@/src/components/Header/header";
 import Footer from "@/src/components/Footer";
+import Header from "@/src/components/Header/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
