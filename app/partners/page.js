ae1f20c9b77b196d0d4d3254fd29261266eb934d
apexlawndivya <djanga@apexlawncompany.com>
2025-10-20 18:39:12 +0530

    partners section

---
+++ app/partners/page.js
@@ -0,0 +1,84 @@
+import Link from "next/link";
+import styles from "./partners.module.css";
+import Image from "next/image";
+
+export default function Partners() {
+  return (
+    <div className={`${styles.parentBlock}`}>
+      <div>
+        <h1 className={`heading-font`} style={{ textAlign: "center", padding: "10px 0" }}>Partners</h1>
+      </div>
+      <div className={`${styles.content}`}>
+        <div className={`margin-right ${styles.productSection}`}>
+          <div className={styles.imageContainer}>
+            <Image
+              src="/assets/partners/apex-lawn-1.jpg"
+              alt="Apex Lawn care"
+              width={634}
+              height={300}
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.textContainer}>
+            <h2 className={`heading-font ${styles.title}`}>
+              Apex Lawn Company
+            </h2>
+            <br />
+            <p className={styles.description}>
+              Apex Lawn Company is a modern, responsive website crafted to
+              showcase professional lawn care and landscaping services. Designed
+              with a clean interface and smooth navigation, it highlights
+              service offerings, client testimonials, and easy online
+              appointment booking. Built with performance and clarity in mind,
+              the site reflects Apex's commitment to quality, reliability, and
+              the art of outdoor beauty.
+            </p>
+            <Link
+              href="https://apexlawncompany.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.link}
+            >
+              Visit Website ↗
+            </Link>
+          </div>
+        </div>
+
+        <div className={`margin-right ${styles.productSection}`}>
+          <div className={styles.imageContainer}>
+            <Image
+              src="/assets/partners/prana-icon.png"
+              alt="Prana Health Care"
+              width={634}
+              height={300}
+              className={styles.image2}
+            />
+          </div>
+          <div className={styles.textContainer}>
+            <h2 className={`heading-font ${styles.title}`}>
+              Prana Health Care
+            </h2>
+            <br />
+            <p className={styles.description}>
+              Prana Health Care is a serene, user-focused website designed to
+              reflect trust, wellness, and compassionate care. With a calming
+              design and clear structure, it presents medical services, expert
+              profiles, and patient resources seamlessly. Optimized for both
+              performance and accessibility, the site helps visitors easily
+              connect with healthcare professionals and book appointments online
+              - embodying Prana's spirit of holistic healing and digital care.
+            </p>
+            <Link
+              href="https://prana.healthcare/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.link}
+            >
+              Visit Website ↗
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
